27c32c8d3ffb48d661f5558f2f468dc2bdf39ae1
hgiraldi <rick.giraldi@gmail.com>
2025-08-29 14:04:52 -0300

    Penha entrando no log

--- z_Complementos/12_Gerar_Etiqueta_Penha.jsx
+++ z_Complementos/12_Gerar_Etiqueta_Penha.jsx
@@ -427,3 +427,105 @@ for (var i = 0; i < cores.length; i++) {
 }
 
 alert("🏁 Etiquetas Criadas!");
+
+// Nome do arquivo de texto que você deseja criar
+var nomeArquivoTxt = nomeScript + "_" + cliente + "_" + serviceOrderNumber + "_" + produtoComUnderline + "_" + resultadoOperador;
+
+// Função para obter o caminho da pasta
+function getFolderPathUtilizacao() {
+    var pastaDestino = "";
+
+    // Check the operating system
+    if ($.os.indexOf("Windows") !== -1) {
+        // Windows folder path
+        pastaDestino = "\\\\192.168.1.15\\uteis\\_Padroes_clientes_Alpha\\_Scripts\\Utilizacao\\Companion\\";
+    } else {
+        // Mac folder path
+        pastaDestino = "/uteis/_Padroes_clientes_Alpha/_Scripts/Utilizacao/Companion/";
+    }
+
+    return pastaDestino;
+}
+
+// Obtém o caminho da pasta
+var pastaDestino = getFolderPathUtilizacao();
+
+var conteudo = '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>' +
+    '<Illustrator Origem ="' + nomeScript + '">' +
+    '<Log Cliente ="' + cliente + '"' +
+    ' Script ="' + nomeScript + '"' +
+    ' Order ="' + serviceOrderNumber + '"' +
+    ' CP ="' + produtoComUnderline + '"' +
+    ' Operador ="' + resultadoOperadorNome + '"/>' +
+    '</Illustrator>'
+
+
+// Cria o objeto File para o arquivo de texto
+var arquivoTxt = new File(pastaDestino + "/" + nomeArquivoTxt + ".xml");
+
+// Cria o arquivo e escreve o conteúdo
+arquivoTxt.open("w");
+arquivoTxt.write(conteudo);
+arquivoTxt.close();
+
+// Function to get platform-specific folder path
+function getFolderPathCopyLog() {
+    var folderPathCopy = "";
+
+    // Check the operating system
+    if ($.os.indexOf("Windows") !== -1) {
+        // Windows folder path
+        folderPathCopy = "\\\\aeserver16\\Engine\\_Jobfolder\\" + serviceOrderNumber + "\\_log\\";
+    } else {
+        // Mac folder path
+        folderPathCopy = "/Engine/_Jobfolder/" + serviceOrderNumber + "/_log/";
+    }
+
+    return folderPathCopy;
+}
+
+// Obtém o caminho para a pasta de destino
+var folderPathCopy = getFolderPathCopyLog();
+
+var nomeArquivoTxtCopy = serviceOrderNumber + "_I_Illustrator_EtiquetaCoresPenha";
+
+// Cria o objeto File para o destino de cópia
+var destinoDaCopia = new File(folderPathCopy + "/" + nomeArquivoTxtCopy + ".xml");
+
+// Copia o arquivo para o destino
+if (arquivoTxt.copy(destinoDaCopia)) {
+
+} else {
+    // alert("VERIFICAR SE ESTA CONECTADO COM O AUTOMATION");
+}
+
+
+//============================CSV====================================//
+
+// Função para obter a data atual em formato "YYYY-MM-DD HH:MM:SS"
+function getCurrentDateTime() {
+    var now = new Date();
+    return now.getFullYear() + "-" +
+        ("0" + (now.getMonth() + 1)).slice(-2) + "-" +
+        ("0" + now.getDate()).slice(-2) + " " +
+        ("0" + now.getHours()).slice(-2) + ":" +
+        ("0" + now.getMinutes()).slice(-2) + ":" +
+        ("0" + now.getSeconds()).slice(-2);
+}
+
+// Dados para o CSV
+var linhaCSV = resultadoOperadorNome + "," +
+    nomeScript + "," +
+    serviceOrderNumber + "," +
+    cliente + "," +
+    banda + "," +
+    "1," +
+    getCurrentDateTime() + "\n";
+
+// Caminho do arquivo CSV
+var arquivoCSV = new File(pastaDestino + "/data_records.csv");
+
+// Abre o arquivo CSV para append (adicionar linha ao final)
+arquivoCSV.open("a");
+arquivoCSV.write(linhaCSV);
+arquivoCSV.close();
